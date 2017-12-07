aba321c1abb1728e0e53ea4267784b5486f10038
Robby Ranshous <rranshous@gmail.com>
2017-12-06 21:51:32 -0500

    network broker publishes from background queue

--- network_broker.rb
+++ network_broker.rb
@@ -1,4 +1,8 @@
 require 'sinatra'
+require 'thread'
+require 'socket'
+
+Thread.abort_on_exception = true
 
 PORT = 7777
 
@@ -7,12 +11,12 @@ def log msg
 end
 
 # does not support binary
-require 'socket'
 class NetworkBroker
-  attr_accessor :server, :receivers, :thread
+  attr_accessor :server, :receivers, :accept_thread, :publish_thread, :to_publish
 
   def initialize
     self.receivers = {}
+    self.to_publish = SizedQueue.new 10
   end
 
   def publish receiver_id, data
@@ -21,10 +25,8 @@ class NetworkBroker
       log "receiver [#{receiver_id}] not found in #{receivers.keys}"
       return false
     else
-      log "writing #{receiver_id}"
-      socket = receivers[receiver_id]
-      socket.write("#{data}\n")
-      socket.flush
+      log "adding to queue [#{receiver_id}]: #{data}"
+      to_publish << [receiver_id, data]
     end
     true
   rescue Errno::EPIPE
@@ -36,7 +38,8 @@ class NetworkBroker
   def start!
     log "started listening on #{PORT}"
     self.server = TCPServer.new PORT
-    self.thread = Thread.new { self.work }
+    self.accept_thread = Thread.new { self.work_accept }
+    self.publish_thread = Thread.new { self.work_publish }
   end
 
   def add_receiver receiver_id, socket
@@ -45,7 +48,19 @@ class NetworkBroker
     self.receivers[receiver_id] = socket
   end
 
-  def work
+  def work_publish
+    loop do
+      receiver_id, data = to_publish.pop
+      log "workign publish #{receiver_id}: #{data}"
+      socket = receivers[receiver_id]
+      log "writing #{receiver_id}: #{data} :: #{socket}"
+      socket.write("#{data}\n")
+      log "flushing #{receiver_id}: #{data}"
+      socket.flush
+    end
+  end
+
+  def work_accept
     log "starting work"
     loop do
       log "new loop"
@@ -57,7 +72,6 @@ class NetworkBroker
       end
     end
   end
-    
 end
 
 set :network_broker, NetworkBroker.new
