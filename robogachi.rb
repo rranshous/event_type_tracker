021edca3794bacb4811271017220ddbbce12f809
Robby Ranshous <rranshous@gmail.com>
2017-12-26 20:17:00 -0500

    refactor

--- robogachi.rb
+++ robogachi.rb
@@ -6,7 +6,7 @@ set :http_port, (ARGV.shift || 8080).to_i
 set :hunger_period_seconds, 60
 
 state_field :last_fed_at, nil, lambda { |s,o| [s,o].compact.max }
-state_field :hunger_times, []
+state_field :times_of_hunger, []
 
 where "action == 'feed'" do |event, state|
   puts "BEING FED"
@@ -15,25 +15,25 @@ end
 
 report 'status' do |state|
   puts "getting status"
-  { hunger_times: state.hunger_times.map(&:iso8601) }
+  { times_of_hunger: state.times_of_hunger.map(&:iso8601) }
 end
 
 periodically do |state|
   if Time.now >= hungry_at(state)
     puts "HUNGRY"
-    state.hunger_times << Time.now
+    state.times_of_hunger << Time.now
   else
     puts "NOT HUNGRY"
   end
 end
 
 cleanup do |state|
-  state.hunger_times = state.hunger_times.uniq.sort
+  state.times_of_hunger = state.times_of_hunger.uniq.sort
 end
 
 def hungry_at state
   if state.last_fed_at.nil?
-    Time.now
+    Time.now - 1
   else
     state.last_fed_at + Config.get(:hunger_period_seconds)
   end
